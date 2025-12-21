68f162517a6d2d0f96ff1f2a246da67ce3609df6
LooseGrip9 <thoriqbusinessthing@gmail.com>
2025-12-21 14:03:02 +0700

    added art for main menu, and done creating main menu

---
+++ Scene/UI/main_menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/Battle/Battle.tscn")
